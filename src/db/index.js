ff0f9bbf23986e72dae00c341c94e7af06e9b499
SavaniShubham <shubham.savani2004@gmail.com>
2024-07-26 20:39:27 +0530

    connect to the mongodb database

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>
+{
+    try {
+       const connectionInstence = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       console.log(`Mongodb connected !! db host :${connectionInstence.connection.host}`)
+        
+    } catch (error) {
+
+        console.log("Mongodb connection Failed : ",error);
+        process.exit(1);
+        
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import connectDB from "./db/index.js";
+// require('dotenv').config({path:'.env'})
+import  dotenv from "dotenv";
+dotenv.config({
+    path:'./env'
+})
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+
+// const app=express();
+
+// (async ()=>
+// {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error",(error)=>
+//         {
+//             console.log("error",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>
+//         {
+//             console.log(`app is listening on port ${process.env.PORT}`);
+//         })
+
+
+        
+//     } catch (error) {
+//         console.error("Error :",error);
+//         throw error
+//     }
+
+// })()
