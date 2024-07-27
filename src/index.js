da4c623fdc690e8ba2adba0b7a67139f3a84ec6d
SavaniShubham <shubham.savani2004@gmail.com>
2024-07-27 22:04:48 +0530

    crate server and more things and crate utils function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import app from "./app.js";
 import connectDB from "./db/index.js";
 // require('dotenv').config({path:'.env'})
 import  dotenv from "dotenv";
@@ -7,7 +8,20 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then( ()=>
+{
+    app.on("error",(error)=>
+    {
+        console.log("server running error :" , error);
+
+    })
+    app.listen(process.env.PORT || 3000,(()=>
+    {
+        console.log(`SERVER IS RUNNING AT PORT ${process.env.PORT}`);
+    }))
+})
+.catch((err)=> console.log("Mongodb connection failed !!!"))
 
 
 
